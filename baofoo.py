e166df3b36f5bd8393d06eb95eaa85d2dfb6e53b
Kelvin <yifeng.liang@accenture.com>
2017-03-22 11:38:01 +0800

    fast commit

--- baofoo.py
+++ baofoo.py
@@ -9,25 +9,43 @@ import tornado.web
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
+        data = dict()
         try:
-          content = str(self.get_argument('content'))
+            data["MemberID"] = str(self.get_argument('MemberID', ''))
+            data["TerminalID"] = str(self.get_argument('TerminalID', ''))
+            data["TransID"] = str(self.get_argument('TransID', ''))
+            data["Result"] = str(self.get_argument('Result', ''))
+            data["ResultDesc"] = str(self.get_argument('ResultDesc', ''))
+            data["FactMoney"] = str(self.get_argument('FactMoney', ''))
+            data["AdditionalInfo"] = str(self.get_argument('AdditionalInfo', ''))
+            data["SuccTime"] = str(self.get_argument('SuccTime', ''))
+            data["Md5Sign"] = str(self.get_argument('Md5Sign', ''))
+            data["BankID"] = str(self.get_argument('BankID', ''))
         except:
             return self.finish({'data': '参数错误', 'code': 500})
-
-        proc = subprocess.Popen("php decrypt.php " + content, shell=True, stdout=subprocess.PIPE)
+        proc = subprocess.Popen("php decrypt.php "
+                                + data["MemberID"] + ' '
+                                + data["TerminalID"] + ' '
+                                + data["TransID"] + ' '
+                                + data["Result"] + ' '
+                                + data["ResultDesc"] + ' '
+                                + data["FactMoney"] + ' '
+                                + data["AdditionalInfo"] + ' '
+                                + data["SuccTime"] + ' '
+                                + data["Md5Sign"] + ' '
+                                + data["BankID"], shell=True,
+                                stdout=subprocess.PIPE)
         script_response = proc.stdout.read()
         return self.finish(json.loads(script_response))
 
     def post(self):
         try:
-            user_id = str(self.get_argument('user_id'))  # 平台USER_ID
-            id_card = str(self.get_argument('id_card'))  # 身份证号码
-            id_holder = str(self.get_argument('id_holder'))  # 姓名
-            txn_amt = str(self.get_argument('txn_amt'))  # 交易金额额
+            PayID = str(self.get_argument('PayID'))
+            Money = str(self.get_argument('Money'))
         except:
             return self.finish({'data': '参数错误', 'code': 500})
 
-        proc = subprocess.Popen("php encrypt.php " + user_id + " " + id_card + " " + id_holder + " " + txn_amt, shell=True,
+        proc = subprocess.Popen("php encrypt.php " + PayID + " " + Money, shell=True,
                                 stdout=subprocess.PIPE)
         script_response = proc.stdout.read()
         return self.finish(json.loads(script_response))
@@ -44,5 +62,3 @@ application = tornado.web.Application([
 if __name__ == "__main__":
     application.listen(8090)
     tornado.ioloop.IOLoop.instance().start()
-
-
